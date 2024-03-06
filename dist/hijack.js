f386c4eb8b60b510bbe0a4ac4001faca13f4d41a
Paxxs <10364775+Paxxs@users.noreply.github.com>
2024-03-07 03:19:07 +0800

    build js file
    - adjust message composition to avoid template literals (reducing the usage `${}` variable formatting to prevent user accidents(eg., when using regular expressions).
    - reformat CSS string for clarity and add z-index to toast container

--- dist/hijack.js
+++ dist/hijack.js
@@ -1,36 +1,36 @@
 (() => {
-  const m = ["closeai.biz"], f = window.fetch, u = window.XMLHttpRequest, w = () => {
-    const o = "#toast-container{position:fixed;top:20px;right:20px;display:flex;flex-direction:column-reverse;gap:10px}.toast{background-color:rgba(0,0,0,.75);font-size:1rem;color:#fff;padding:5px;border-radius:5px;animation:4s forwards fadeOut}@keyframes fadeOut{from{opacity:1}to{opacity:0}}", e = document.createElement("style");
-    e.textContent = o, document.head.appendChild(e);
+  const f = ["closeai."], m = window.fetch, u = window.XMLHttpRequest, w = () => {
+    const e = "#toast-container{position:fixed;top:20px;right:20px;display:flex;flex-direction:column-reverse;gap:10px;z-index:50;}.toast{background-color:rgba(0,0,0,.75);font-size:1rem;color:#fff;padding:5px;border-radius:5px;animation:4s forwards fadeOut}@keyframes fadeOut{from{opacity:1}to{opacity:0}}", o = document.createElement("style");
+    o.textContent = e, document.head.appendChild(o);
   }, r = (() => {
-    const o = document.createElement("div");
-    return o.id = "toast-container", document.body.appendChild(o), w(), o;
-  })(), i = (o) => {
     const e = document.createElement("div");
-    e.className = "toast", e.textContent = o, r.appendChild(e), setTimeout(() => {
-      r.removeChild(e);
+    return e.id = "toast-container", document.body.appendChild(e), w(), e;
+  })(), i = (e, o, t = !1) => {
+    const n = document.createElement("div");
+    n.className = "toast", n.textContent = (t ? "🪬隐私防护 " : "🫥外部请求 ") + o + " " + e.replace(/^https?:\/\//, ""), r.appendChild(n), setTimeout(() => {
+      r.removeChild(n);
     }, 4e3);
-  }, l = (o) => m.some((e) => o.includes(e)), n = (o, e, t = "normal") => {
-    t === "external" ? i(`🫥 外部${o}-${e.replace(/^https?:\/\//, "")}`) : t === "intercepted" && i(`🛡️ 隐私保护${o}-${e.replace(/^https?:\/\//, "")}`), console.log(`${t} ${o} ${e}`);
-  }, g = (o) => (o && console.log(`SpyRBody:${o}`), new Response(JSON.stringify({ message: ":)" }), {
+  }, d = (e) => f.some((o) => e.includes(o)), s = (e, o, t = "normal") => {
+    t === "external" ? i(o, e) : t === "intercepted" && i(o, e, !0), console.log(t, e, o);
+  }, g = (e) => (e && console.log("SpyRBody:", e), new Response(JSON.stringify({ m: ":)" }), {
     status: 200,
     headers: { "Content-Type": "application/json" }
   }));
-  window.fetch = async (o, e) => {
-    const t = typeof o == "string" ? o : o instanceof URL ? o.href : o.url, s = (e == null ? void 0 : e.method) || "GET", a = new URL(t, window.location.origin).hostname, c = window.location.hostname;
-    return l(a) ? (n(s, t, "intercepted"), Promise.resolve(g(e == null ? void 0 : e.body))) : (a !== c ? n(s, t, "external") : n(s, t, "normal"), f(o, e));
+  window.fetch = async (e, o) => {
+    const t = typeof e == "string" ? e : e instanceof URL ? e.href : e.url, n = (o == null ? void 0 : o.method) || "GET", a = new URL(t, window.location.origin).hostname, c = window.location.hostname;
+    return d(a) ? (s(n, t, "intercepted"), Promise.resolve(g(o == null ? void 0 : o.body))) : (a !== c ? s(n, t, "external") : s(n, t, "normal"), m(e, o));
   };
-  class h extends u {
-    open(e, t, s = !0, d, a) {
-      const c = new URL(t, window.location.origin), R = c.hostname !== window.location.hostname;
-      if (l(c.hostname)) {
-        n(e, t, "intercepted"), this.send = (p) => {
-          p && console.log(`SpyRBody:${p}`);
+  class x extends u {
+    open(o, t, n = !0, l, a) {
+      const c = new URL(t, window.location.origin), h = c.hostname !== window.location.hostname;
+      if (d(c.hostname)) {
+        s(o, t, "intercepted"), this.send = (p) => {
+          p && console.log("SpyXBody:", p);
         };
         return;
       }
-      R ? n(e, t, "external") : n(e, t), super.open(e, t, s, d, a);
+      h ? s(o, t, "external") : s(o, t), super.open(o, t, n, l, a);
     }
   }
-  window.XMLHttpRequest = h;
+  window.XMLHttpRequest = x;
 })();
